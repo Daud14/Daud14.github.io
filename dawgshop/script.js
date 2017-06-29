a0e10698d2a05b96b25a0c7fdb0b02c13d315880
Daud Malik <daud@ymail.com>
2017-06-29 18:23:08 +0000

    Dawg it up

--- dawgshop/script.js
+++ dawgshop/script.js
@@ -1,5 +1,5 @@
 document.getElementById('go').onclick = function() {
     var inputText = $('#text-input').val();
-    var dawgifiedText = inputText + ", dawg";
+    var dawgifiedText = inputText + ", savage";
     document.getElementById('output').innerHTML = dawgifiedText;
 };
